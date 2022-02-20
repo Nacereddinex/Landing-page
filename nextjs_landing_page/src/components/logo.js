ac986220b1f0f430ae27b1ef25b821e5da4dc1f4
Nacereddinex <38004861+Nacereddinex@users.noreply.github.com>
2022-02-20 20:17:52 +0100

    first commit

---
+++ nextjs_landing_page/src/components/logo.js
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { jsx, Image } from 'theme-ui';
+import { Link } from 'components/link';
+import { displayName } from 'react-stickynode/dist/Sticky';
+
+export default function Logo({ src, ...rest }) {
+  return (
+    <Link path="/" sx={{ 
+      variant:'links.logo' ,
+      display:'flex',
+      cursor:'pointer',
+      mr:15,}} {... rest} >
+      <Image src={src} alt="next js landing page logo"/>
+    </Link>
+  );
+}
